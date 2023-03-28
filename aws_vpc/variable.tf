3dce3a49ff9d2d726cdddb20613369af391a4d7b
noela <dohnoela59@gmail.com>
2023-03-27 20:05:08 -0400

    creating ws vpc module

---
+++ aws_vpc/variable.tf
@@ -0,0 +1,44 @@
+variable "vpc_cidr" {
+  description = "value"
+  type        = string
+}
+
+#variable "pub_sub_1" {
+ # description = "value"
+ # type        = string
+#}
+#variable "pub_sub_2" {
+ # description = "value"
+  #type        = string
+#}
+variable "pub_sub_1_tags" {
+  description = "value"
+  type        = map(string)
+}
+variable "pub_sub_2_tags" {
+  description = "value"
+  type        = map(string)
+}
+
+variable "aws_vpc_tags" {
+  description = "value"
+  type        = map(string)
+}
+variable "az" {
+  description = "value"
+  type        = string
+}
+#variable "vpc_cidr" {
+ # description = "value"
+  #type        = string
+#}
+variable "pub_sub_1_cidr" {
+  description = "value"
+  type        = string
+}
+variable "pub_sub_2_cidr" {
+  description = "value"
+  type        = string
+}
+
+
